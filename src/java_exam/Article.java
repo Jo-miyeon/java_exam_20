4b6198bbefd9ecefc9edadb3164a3eff3482985e
Jo-miyeon <my0v0@naver.com>
2020-10-21 11:53:42 +0900

    article 수정

--- src/java_exam/Article.java
+++ src/java_exam/Article.java
@@ -1,7 +1,27 @@
 package java_exam;
 
 public class Article {
-	int id;
-	String title;
-	String body;
+	private int id;
+	private String title;
+	private String body;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	
 }
--- src/java_exam/exam_3.java
+++ src/java_exam/exam_3.java
@@ -7,56 +7,63 @@ public class exam_3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str;
-		ArrayList<String> titles = new ArrayList<>();
-		ArrayList<String> bodies = new ArrayList<>();
-		ArrayList<Integer> ids = new ArrayList<>();
-		ArrtayList<Article> articles = new ArrayList<>();
+		ArrayList<Article> articles = new ArrayList<>(); //arraylist하나로 쓸수있다,
 		
 		int no=1;
 		int targetId;
 		while(true) {
+			
 			System.out.print("명령어를 입력해주세요:");
 			str=sc.nextLine();
 			if(str.equals("exit")) {
+				System.out.println("종료되었습니다.");
 				break;
 			}else if(str.equals("add")) {
-				
 				Article a = new Article();
-				a.id=no;
+				a.setId(no); //아이디 타이틀등을 따로 저장안해도됨
 				no++;
 				System.out.print("게시물 제목을 입력해주세요:");
 				String title = sc.nextLine();
-				a.title = title;
+				a.setTitle(title);
+				
 				System.out.print("게시물 내용을 입력해주세요:");
 				String body = sc.nextLine();
-				a.body = body;
+				a.setBody(body);
 				
 				articles.add(a);
 				System.out.println("게시물이 등록되었습니다.");
 			
 		
 			}else if(str.equals("list")) {
-				for(int i=0;i<ids.size();i++) {
-					System.out.println("번호:"+ids.get(i));
-					System.out.println("제목:"+titles.get(i));
-					System.out.println("내용:"+bodies.get(i));
+				for(int i=0;i<articles.size();i++) {
+					Article article = articles.get(i); //i번째 articles를 변수에 저장
+					System.out.println("번호:"+article.getId());
+					System.out.println("제목:"+article.getTitle());
+					//System.out.println("내용:"+article.getBody());
 					System.out.println("=====================");
 				}
 			}else if(str.equals("update")) {
 				System.out.print("수정할 게시물번호 선택해주세요: ");
 				targetId = sc.nextInt();
-				for(int i=0;i<ids.size();i++) {
-					int id = ids.get(i);
+				for(int i=0;i<articles.size();i++) {
+					Article article = articles.get(i);
+					int id = articles.get(i).getId();
+					
 					if(id==targetId) {
 						System.out.print("게시물 제목을 입력해주세요:");
 						String newTitle = sc.nextLine();
-						titles.add(newTitle);
+
 						System.out.print("게시물 내용을 입력해주세요:");
 						String newBody = sc.nextLine();
-						bodies.add(newBody);
+						
 						System.out.println("게시물이 등록되었습니다.");
-						titles.set(i,newTitle);
-						bodies.set(i,newBody);
+						
+						Article newArticle = new Article();
+						newArticle.setId(id);
+						newArticle.setTitle(newTitle);
+						newArticle.setBody(newBody);
+						
+						articles.set(i, newArticle);
 						break;
 					}
 				}
@@ -65,25 +72,41 @@ public class exam_3 {
 				targetId = sc.nextInt();
 				int existFlag=2;//1.있음 2.없음
 				
-				for(int i=0; i<ids.size(); i++) {
-					int id = ids.get(i);
+				for(int i=0; i<articles.size(); i++) {
+					int id = articles.get(i).getId();
 					if(id==targetId) {
 						existFlag=1;
-						ids.remove(i);
-						titles.remove(i);
-						bodies.remove(i);
+						articles.remove(i);
+						break;
 					}
 				}
 				if(existFlag==2) {
-					System.out.println("게시물이 존재하지 않습니다");//for문밖ㅇㅔ서 해야함
+					System.out.println("게시물이 존재하지 않습니다.");
 				}else {
 					System.out.println("게시물이 삭제되었습니다.");
-					for(int i=0;i<ids.size();i++) {
-						System.out.println("번호:"+ids.get(i));
-						System.out.println("제목:"+titles.get(i));
-						System.out.println("내용:"+bodies.get(i));
+					for(int i=0;i<articles.size();i++) {
+						Article article = articles.get(i);
+						System.out.println("번호:"+article.getId());
+						System.out.println("제목:"+article.getTitle());
+						//System.out.println("내용:"+article.getBody());
 						System.out.println("=====================");
 					}
+				}
+			}else if(str.equals("read")) {
+				System.out.println("원하는 게시물의 번호를 선택해주세요:");
+				targetId = sc.nextInt();
+				int existFlag = 2;
+				for(int i=0;i<articles.size();i++) {
+					int id = articles.get(i).getId();
+					Article article = articles.get(i);
+					if(id==targetId) {
+						existFlag=1;
+						System.out.println("====="+article.getId()+"=====번 게시물");
+						System.out.println("제목:"+article.getTitle());
+						System.out.println("내용:"+article.getBody());
+					}
+				}if(existFlag==2) {
+					System.out.println("게시물이 존재하지 않습니다.");
 				}
 			}
 		} 
